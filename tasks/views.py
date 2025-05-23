9bdb4c2f5b73d9aa1571fa18ee111493ed1fcf6a
ihortok <itokaryk@gmail.com>
2025-05-23 18:42:01 +0200

    tasks view

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
 
-# Create your views here.
+def task_list(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        if title:
+            Task.objects.create(title=title)
+        return redirect('task_list')
+
+    tasks = Task.objects.all().order_by('-created_at')
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})

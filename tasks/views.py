0cdb35c500adbbf248fc0826b986c7d70dc44cec
ihortok <itokaryk@gmail.com>
2025-06-07 12:17:48 +0200

    tasks complete & delete

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404
 from .models import Task
 
 def task_list(request):
@@ -10,3 +11,14 @@ def task_list(request):
 
     tasks = Task.objects.all().order_by('-created_at')
     return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.completed = not task.completed
+    task.save()
+    return redirect('task_list')
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect('task_list')

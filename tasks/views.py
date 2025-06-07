42dac53ee54e65ffe1b07e916a09a611c48b60b7
ihortok <itokaryk@gmail.com>
2025-06-07 13:20:14 +0200

    users

--- tasks/views.py
+++ tasks/views.py
@@ -1,24 +1,28 @@
 from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Task
 
+@login_required
 def task_list(request):
     if request.method == 'POST':
         title = request.POST.get('title')
         if title:
-            Task.objects.create(title=title)
+            Task.objects.create(title=title, user=request.user)
         return redirect('task_list')
 
-    tasks = Task.objects.all().order_by('-created_at')
+    tasks = Task.objects.filter(user=request.user).order_by('-created_at')
     return render(request, 'tasks/task_list.html', {'tasks': tasks})
 
+@login_required
 def complete_task(request, task_id):
-    task = get_object_or_404(Task, id=task_id)
+    task = get_object_or_404(Task, id=task_id, user=request.user)
     task.completed = not task.completed
     task.save()
     return redirect('task_list')
 
+@login_required
 def delete_task(request, task_id):
-    task = get_object_or_404(Task, id=task_id)
+    task = get_object_or_404(Task, id=task_id, user=request.user)
     task.delete()
     return redirect('task_list')
